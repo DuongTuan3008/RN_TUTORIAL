9c429a6a6e1970cba97d3aac21ce860bdcedc947
“Duongtuan“ <duongqtuan3008@gmail.com>
2019-12-19 21:28:22 +0700

    first day

--- SOURCE/RN_BASE/app/screens/UserScreen.js
+++ SOURCE/RN_BASE/app/screens/UserScreen.js
@@ -1,30 +1,67 @@
 import React, {Component} from 'react';
-import {Text, View, TouchableOpacity, Alert, Image} from 'react-native';
-import i18 from '@i18';
-import * as theme from '@theme';
-import R from '@R';
-export default class UserScreen extends Component {
-  async componentDidMount() {}
+import {Text, View, SafeAreaView, Image} from 'react-native';
 
+export default class UserScreen extends Component {
   render() {
     return (
-      <View
+      <SafeAreaView
         style={{
           flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
+          backgroundColor: '#FFFFFF',
         }}>
-        <TouchableOpacity>
-          <Text>{R.strings.home}</Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            backgroundColor: 'white',
+          }}>
           <Image
             style={{
-              width: 100,
-              height: 100,
+              width: 50,
+              height: 50,
+              backgroundColor: 'blue',
+              borderRadius: 25,
+              marginRight: 11,
+              marginLeft: 25,
+              marginBottom: 12,
+              marginTop: 12,
             }}
-            source={R.images.user}
+            source={require('../assets/images/ic_home.png')}
           />
-        </TouchableOpacity>
-      </View>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              backgroundColor: 'white',
+            }}>
+            <View
+              style={{
+                width: '100%',
+                flexDirection: 'row',
+                backgroundColor: 'white',
+              }}>
+              <Text
+                style={{
+                  fontWeight: 'bold',
+                  flex: 1,
+                  alignContent: 'center',
+                }}>
+                Nguyễn Thị Thu Phương
+              </Text>
+              <Text
+                style={{
+                  color: 'white',
+                  backgroundColor: '#EA4335',
+                  fontSize: 12,
+                  overflow: 'hidden',
+                  borderRadius: 3,
+                }}>
+                Đại lý
+              </Text>
+            </View>
+            <Text>Chỉnh sửa thông tin</Text>
+          </View>
+        </View>
+      </SafeAreaView>
     );
   }
 }
